a60ddd0621630c22841e8ad2cfdf09efbd7fd13e
rafopm <rafopm@gmail.com>
2023-05-11 20:50:28 -0500

    api completed

--- api/src/main/java/alura/one/api/domain/usuario/DatosListadoUsuario.java
+++ api/src/main/java/alura/one/api/domain/usuario/DatosListadoUsuario.java
@@ -4,12 +4,12 @@ public record DatosListadoUsuario(
         Long id_usuario,
         String nombre,
         String correo_electronico,
-        String contrasena,
+        //String contrasena,
         Boolean activo
 ) {
     public DatosListadoUsuario(Usuario usuario) {
         this(usuario.getId_usuario(), usuario.getNombre(), usuario.getCorreo_electronico(),
-                usuario.getContrasena(), usuario.getActivo());
+                 usuario.getActivo());
 
     }
 }
