52a12bf7721a2a320eb91f7ac7a1a3aa254f1720
rafopm <rafopm@gmail.com>
2023-05-22 22:23:43 -0500

    Actualizar Listar topicos ordenado idusuario agregado

--- api/src/main/java/alura/one/api/domain/topico/DatosListadoTopico.java
+++ api/src/main/java/alura/one/api/domain/topico/DatosListadoTopico.java
@@ -1,8 +1,8 @@
 package alura.one.api.domain.topico;
 
-public record DatosListadoTopico(Long idtopico, String titulo, String mensaje, String fechacreacion, Estatus estatus, String usuarionombre, String cursonombre) {
+public record DatosListadoTopico(Long idtopico, String titulo, String mensaje, String fechacreacion, Estatus estatus, String usuarionombre, Long idusuario, String cursonombre) {
     public DatosListadoTopico(Topico topico) {
         this(topico.getIdtopico(), topico.getTitulo(), topico.getMensaje(), topico.getFechacreacion(), topico.getEstatus(),
-                topico.getUsuario().getNombre(), topico.getCurso().getNombre());
+                topico.getUsuario().getNombre(), topico.getUsuario().getIdusuario(), topico.getCurso().getNombre());
     }
 }
